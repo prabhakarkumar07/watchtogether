c90f6e509eb9d808a572f56438b48db68aa04c0e
prabhakarkumar07 <prabhakarkumarekc477@gmail.com>
2026-07-18 00:07:09 +0530

    speed tracker

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ToastProvider>
       <App />
+      <SpeedInsights />
     </ToastProvider>
   </React.StrictMode>
 )
